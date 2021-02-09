37f2ff15ba3f9fb5ac3fba5bd572601830f03f40
Kulwinder Rupal <Kulwinder_Rupal@yahoo.com>
2021-02-10 09:16:22 +1100

    Few styling changes

--- src/App.test.js
+++ src/App.test.js
@@ -3,16 +3,16 @@ import { fireEvent, render } from '@testing-library/react';
 import App from './App';
 
 describe('App Component', () => {
-  test('renders component successfully', () => {
-    const {container} = render(<App />);  
-    expect(container).toMatchSnapshot();
-  });
-  
-  test('Click Read more button and display more cards', () => {
-    const {getByTestId, getAllByTestId} = render(<App />);  
-    const btn = getByTestId('read-more');
-    fireEvent.click(btn);
-    expect(getAllByTestId('card')).toHaveLength(6);
-  });
-})
+    test('renders component successfully', () => {
+        const { container } = render(<App />);
+        expect(container).toMatchSnapshot();
+    });
+
+    test('Click Read more button and display more cards', () => {
+        const { getByTestId, getAllByTestId } = render(<App />);
+        const btn = getByTestId('read-more');
+        fireEvent.click(btn);
+        expect(getAllByTestId('card')).toHaveLength(6);
+    });
+});
 
